86bac81822580ea944629eaae3af1842a2d86f69
ic-27 <isaac.c@tutanota.com>
2024-02-29 16:01:22 -0800

    Put fft output into 8 frequency bands

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -18,6 +18,8 @@
 #define WAV_METADATA_LEN 44
 #define FFTLEN 512
 
+#define MAX_INT16 32767
+
 FIL file_h;
 arm_rfft_fast_instance_f32 s;
 
@@ -44,10 +46,6 @@ int main(void)
 {
     Executor exec = Executor();
     exec.init();
-  
-    //int i;
-    // button_setup();
-    // gpio_setup();
 
     Oled oled = Oled();
     oled.init();
@@ -56,46 +54,69 @@ int main(void)
     f_mount(&Fatfs, "", 0);
 
 #warning check the status
-    FRESULT fileStatus = f_open(&file_h, "1kHz.wav", FA_OPEN_EXISTING | FA_READ);
+    FRESULT fileStatus = f_open(&file_h, "10kHz.wav", FA_OPEN_EXISTING | FA_READ);
 
     // read the wav file in buffer
     int16_t readBuff[FFTLEN]; // 16-bit audio
     UINT bytesRead = 0;
     f_lseek(&file_h, WAV_METADATA_LEN); // ignore any metadata
     
-    float32_t fftIn[FFTLEN]={0}, fftOut[FFTLEN]={0};
-    uint8_t powerFFT[FFTLEN/2]={0};
-    float32_t realFFT[FFTLEN/2]={0}, imagFFT[FFTLEN/2]={0};
+    float32_t fftIn[FFTLEN]={0}, fftOut[FFTLEN]={0}, realFFT[FFTLEN/2]={0}, imagFFT[FFTLEN/2]={0};
+    float32_t powerFFT[FFTLEN/2]={0};
+    float32_t freqBand[8] = {0};
 
     arm_status armStat = arm_rfft_fast_init_f32(&s, FFTLEN); 
     
     do {
 	fileStatus = f_read(&file_h, readBuff, FFTLEN*2, &bytesRead); // read wav file
 
-	for (uint16_t j=0; j<FFTLEN; ++j) { // convert read wav file data into fft input
+	// convert read wav file data into fft input, must be float32_t datatype
+	for (uint16_t j=0; j<FFTLEN; ++j) { 
 	    fftIn[j] = (float32_t)(readBuff[j]);
 	}
-	
-	arm_rfft_fast_f32(&s, fftIn, fftOut, 0); // perform FFT
+	arm_rfft_fast_f32(&s, fftIn, fftOut, 0);
 
-	float32_t peakVal = 0; // calculate the peakHz
-	uint32_t peakHz = 0;
-	uint32_t freqIndex = 0;
+	//float32_t peakVal = 0; // calculate the peakHz
+	//uint32_t peakHz = 0;
+
+	// calculate the magnitude in store into frequency bands
+	uint32_t freqBinIndex = 0;
 	for (uint32_t j=0; j<(FFTLEN/2)-1; ++j) {
 	    realFFT[j] = fftOut[j*2];
 	    imagFFT[j] = fftOut[(j*2)+1];
 
-#warning how to constrain the magnitude to be between 0 and 1 (0 and 64)
 	    // calculate the magnitude
-	    float32_t curMag = sqrtf((realFFT[j]*realFFT[j]) + (imagFFT[j]*imagFFT[j]));
-	    powerFFT[j] = (uint8_t)(curMag*64*2/(32767*FFTLEN));
-	    if (curMag > peakVal) {
-		peakVal = curMag;
-		peakHz = (uint16_t)(freqIndex*44100/((float32_t)FFTLEN));
+	    float32_t mag = sqrtf((realFFT[j]*realFFT[j]) + (imagFFT[j]*imagFFT[j]));
+	    
+	    // scale magnitude between 0 and 1
+	    float32_t scaled_mag = (mag*2/(MAX_INT16*FFTLEN));
+	    powerFFT[j] = scaled_mag;
+
+	    // put into frequency bands
+	    if (freqBinIndex <= 1) {
+		freqBand[0] += scaled_mag;
+	    } else if (freqBinIndex > 1 && freqBinIndex <= 3) {
+		freqBand[1] += scaled_mag;
+	    } else if (freqBinIndex > 3 && freqBinIndex <= 7) {
+		freqBand[2] += scaled_mag;
+	    } else if (freqBinIndex > 3 && freqBinIndex <= 14) {
+		freqBand[3] += scaled_mag;
+	    } else if (freqBinIndex > 14 && freqBinIndex <= 30) {
+		freqBand[4] += scaled_mag;
+	    } else if (freqBinIndex > 30 && freqBinIndex <= 64) {
+		freqBand[5] += scaled_mag;
+	    } else if (freqBinIndex > 64 && freqBinIndex <= 136) {
+		freqBand[6] += scaled_mag;
+	    } else if (freqBinIndex > 136 && freqBinIndex <= 291) {
+		freqBand[7] += scaled_mag;
 	    }
-	    ++freqIndex;
+	    ++freqBinIndex;
 	}
-	freqIndex = 0; // filler!!!
+	// map between 0 and OLED_SCREEN_ROWS (64)
+	
+	// output to oled screen
+	
+	memset(freqBand, 0, sizeof(freqBand));
     } while ((FR_OK == fileStatus) && (bytesRead == FFTLEN*2));
 
 
@@ -110,22 +131,9 @@ int main(void)
     // float32_t maxValue=0;
     // arm_max_f32(&powerFFT[1], (FFTLEN/2)-1, &maxValue, &maxIndex);
 
-    
-    /* Blink the LED (PD12) on the board. */
+
     while (1) {
 	
-	// gpio_toggle(GPIOD, GPIO12);
-
-	// /* Upon button press, blink more slowly. */
-	// if (gpio_get(GPIOA, GPIO0)) {
-	//     for (i = 0; i < 3000000; i++) {	/* Wait a bit. */
-	// 	__asm__("nop");
-	//     }
-	// }
-
-	// for (i = 0; i < 3000000; i++) {		/* Wait a bit. */
-	//     __asm__("nop");
-	// }
     }
 
     return 0;
